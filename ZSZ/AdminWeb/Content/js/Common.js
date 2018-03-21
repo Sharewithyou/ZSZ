384b4a38118369b9ab1b549077896b02f3f3f8ca
Share <share_wei@163.com>
2018-03-21 11:47:30 +0800

    修改公共提交

--- ZSZ/AdminWeb/Content/js/Common.js
+++ ZSZ/AdminWeb/Content/js/Common.js
@@ -1,16 +1,28 @@
-﻿var AjaxUtil = {
+﻿
+
+
+
+
+
+var AjaxUtil = {
 
     // 基础选项
     options: {
-        method: "post", // 默认提交的方法,post
-        url: "", // 请求的路径 required
-        params: {}, // 请求的参数
-        type: 'json', // 默认返回的内容的类型json
-        callback: function () {
+        url: "", // 请求的路径 required,
+        type: "post",
+        data: {},
+        dataType: 'json',
+        success: function () {
 
-        }// 回调函数 required
+        },
+        complete: function () {
+
+        },
+        error: function () {
+
+        }
     },
-  
+
     // 设置基础选项
     setOptions: function (newOptions) {
         for (var pro in newOptions) {
@@ -23,15 +35,14 @@
         var params = this.options.params;
         for (var pro in params) {
             var paramValue = params[pro];
-            /*if(this.options.method.toUpperCase() === "GET")
-            {
+            if (this.options.method.toUpperCase() === "GET") {
                 paramValue = encodeURIComponent(params[pro]);
-            }*/
+            }
             paramsArray.push(pro + "=" + paramValue);
         }
         return paramsArray.join("&");
     },
- 
+
 
     // 发送Ajax请求
     request: function (options) {
@@ -40,36 +51,21 @@
         // 设置参数
         ajaxObj.setOptions.call(ajaxObj, options);
 
-        // 创建XMLHttpRequest对象
-        var xmlhttp = ajaxObj.createRequest.call(ajaxObj);
-
-        // 设置回调函数
-        xmlhttp.onreadystatechange = function () {
-            ajaxObj.readystatechange.call(ajaxObj, xmlhttp);
-        };
-
-        // 格式化参数
-        var formateParams = ajaxObj.formateParameters.call(ajaxObj);
-
         // 请求的方式
         var method = ajaxObj.options.method;
         var url = ajaxObj.options.url;
 
         if ("GET" === method.toUpperCase()) {
+            // 格式化参数
+            var formateParams = ajaxObj.formateParameters.call(ajaxObj);
             url += "?" + formateParams;
         }
 
-        // 建立连接
-        xmlhttp.open(method, url, true);
+        $.ajax({
+            url: ajaxObj.options.url,
+            type:
+        })
 
-        if ("GET" === method.toUpperCase()) {
-            xmlhttp.send(null);
-        } else if ("POST" === method.toUpperCase()) {
-            // 如果是POST提交，设置请求头信息
-            xmlhttp.setRequestHeader("Content-Type",
-                "application/x-www-form-urlencoded");
-            xmlhttp.send(formateParams);
-        }
     }
 };
 
