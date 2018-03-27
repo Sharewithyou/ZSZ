587136cc125a930ae6f332893d56c067656b57f8
Share <share_wei@163.com>
2018-03-27 10:17:11 +0800

    开始菜单树的封装

---
+++ ZSZ/AdminWeb/Controllers/SysPermissionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminWeb.Controllers
+{
+    public class SysPermissionController : Controller
+    {
+        // GET: SysPermission
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
