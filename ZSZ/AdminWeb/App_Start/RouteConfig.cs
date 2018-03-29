4e97708053c5d137b09243f6991a8d332ed14945
Share <share_wei@163.com>
2018-03-29 18:47:50 +0800

    站点地图的测试

--- ZSZ/AdminWeb/App_Start/RouteConfig.cs
+++ ZSZ/AdminWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AdminWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "InitData", action = "InitOperationData", id = UrlParameter.Optional }
+                defaults: new { controller = "InitData", action = "Test", id = UrlParameter.Optional }
             );
         }
     }
